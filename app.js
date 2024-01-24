2287d69151d2ade12cc4f792cb7ce160d3fbfbe8
Angel Gonzalez <116753348+gonsar2211@users.noreply.github.com>
2024-01-24 09:36:19 -0500

    actividad 1 desafío para practicar

---
+++ app.js
@@ -0,0 +1,19 @@
+alert('Welcome to our Website');
+
+// let name = "Luna";
+// let age = 25;
+// let salesNumber = 50;
+// let availableBalance = 1000;
+
+// alert('Mistake! Complete all fields');
+
+let errorMessage = 'Mistake! Complete all fields';
+
+alert(errorMessage);
+
+let name = prompt('what is your username?');
+let age = prompt('enter your age');
+    if (age >= 18) {
+        alert('You can get your drivers license!')
+    };
+
